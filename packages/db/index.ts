ca9b25a3394fc144c777c81b2800c7078b72d01d
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-09-18 22:43:54 +0530

    prisma working fine

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { PrismaClient } from "./generated/prisma";
+const prisma = new PrismaClient()
+
+await prisma.user.create({
+    data: {
+        name: "John Dough",
+        email: `john-${Math.random()}@example.com`,
+    },
+});
+
+
