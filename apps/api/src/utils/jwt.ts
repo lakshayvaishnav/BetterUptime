be11b896094d9841023306595ad819a256d11daf
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-09-27 11:54:00 +0530

    added generate token and verify token

--- apps/api/src/utils/jwt.ts
+++ apps/api/src/utils/jwt.ts
@@ -1,13 +1,20 @@
 import jwt from "jsonwebtoken";
+import type { JWTPayload } from "../types";
 
 const JWT_SECRET = process.env.JWT_SECRET;
+
 if (!JWT_SECRET) throw new Error("jwt secret env var is required");
 
-
-export function signToken(payload: object, opts?: jwt.SignOptions) {
-    return jwt.sign(payload, JWT_SECRET, { expiresIn: "2h", ...(opts || {}) })
+export const generateToken = (payload: JWTPayload): string => {
+    const res = jwt.sign(payload, JWT_SECRET, {
+        expiresIn: "2h"
+    })
+    console.debug("✅ token generated :", res);
+    return res;
 }
 
-export function verifyToken<T = any>(token: string): T {
-    return jwt.verify(token, JWT_SECRET) as T;
+export const verifyToken = (token: string): JWTPayload => {
+    const res = jwt.verify(token, JWT_SECRET) as JWTPayload;
+    console.debug("✅ token verified : ", res);
+    return res;
 }
