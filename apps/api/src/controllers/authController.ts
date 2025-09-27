f38717b02a58d4d360c308c0a5be31a21ce37baa
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2025-09-27 16:14:04 +0530

    added singup controller

---
+++ apps/api/src/controllers/authController.ts
@@ -0,0 +1,76 @@
+import type { Request, Response } from "express";
+import z, { success } from "zod";
+import userModel from "../models/userModel";
+import { generateToken, getCookieOptions } from "../utils/jwt";
+import type { LoginRequest, SignupRequest, AuthenticatedRequest } from "../types";
+
+class AuthController {
+    async signup(req: Request, res: Response): Promise<void> {
+        try {
+            const { email, name, password }: SignupRequest = req.body;
+
+            // check if user already exists
+            const existingUser = await userModel.getUserByEmail(email);
+            if (existingUser) {
+                res.status(400).json({
+                    success: false,
+                    message: "user already exists with this email"
+                })
+                return;
+            }
+
+            // create new user
+            const user = await userModel.createUser({
+                email: email,
+                name: name,
+                password: password
+            })
+
+            if (!user) {
+                res.status(400).json({
+                    success: false,
+                    message: "failed to create user"
+                })
+                return;
+            }
+
+            // generate jwt token
+            const token = generateToken({
+                email: user.email,
+                id: user.id
+            })
+
+            // set cookie
+            res.cookie('token', token, getCookieOptions());
+
+            res.status(201).json({
+                success: true,
+                message: "user registered successfully",
+                data: {
+                    user: {
+                        id: user.id,
+                        email: user.email,
+                        name: user.name
+                    },
+                    token
+                }
+            });
+
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: "Internal server error",
+                error: process.env.NODE_ENV === "development" ? error : undefined
+            });
+        }
+    }
+    async login(req: Request, res: Response): Promise<void> {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async
+}
