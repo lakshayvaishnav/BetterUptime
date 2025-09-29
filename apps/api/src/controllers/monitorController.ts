70e3b25f846490ca766b419391c6cdc4b106e46a
lakshayvaishnav <thelakshayvaishnav@gmail.com>
2025-09-29 11:32:37 +0530

    adde create user monitor controller

--- apps/api/src/controllers/monitorController.ts
+++ apps/api/src/controllers/monitorController.ts
@@ -1,13 +1,52 @@
-import type { AuthenticatedRequest } from "../types";
+import { success } from "zod";
+import monitorModel from "../models/monitorModel";
+import type { AuthenticatedRequest, CreateMonitor } from "../types";
+import type { Response } from "express";
 
 class MonitorController {
     async createUserMonitor(req: AuthenticatedRequest, res: Response): Promise<void> {
         try {
             const { id } = req.params;
-            
-        } catch (error) {
+            let { userId, url }: CreateMonitor = req.body;
 
+            if (!id) {
+                res.status(400).json({
+                    success: false,
+                    message: "invalid params id does not exists"
+                })
+                return;
+            }
+
+            userId = id;
+
+            if (!url) {
+                res.status(400).json({
+                    success: false,
+                    message: "invalid url or url does not exist"
+                })
+            }
+
+            const monitor = await monitorModel.createUserMontior({ userId: id, url });
+
+            console.debug("✅ monitor created successfully in controler : ", monitor);
+
+            res.json({
+                success: true,
+                data: {
+                    monitor
+                }
+            })
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: "Internal server error"
+            })
+            console.debug("error in create user monitor : ", error);
         }
+    }
+
+    async deleteUserMonitor(req : AuthenticatedRequest, res :Response) : Promise<void> {
+        
     }
 }
 
